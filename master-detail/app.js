ea39f1c083dcbc3d8f2b83461d70b042050afcea
dani <danivicario@dvicario.es>
2019-09-13 17:27:55 +0200

    added

--- master-detail/app.js
+++ master-detail/app.js
@@ -31,6 +31,7 @@ app.get("/", (req, res) => {
   res.redirect("/master");
 });
 
+// get all movies in the chosen sort order (asc or desc)
 app.get("/master/:sortOrder?", (req, res) => {
   // if (req.params.sortOrder)
   if (
@@ -76,6 +77,7 @@ app.get("/master/:sortOrder?", (req, res) => {
   }
 });
 
+// get a specific movie
 app.get("/movie/:id", (req, res) => {
   Movies.findById(req.params.id).then(oneMovie => {
     res.render("movie-detail", { oneMovie, host: process.env.HOST });
@@ -86,12 +88,14 @@ app.get("/movie/:id", (req, res) => {
 //   res.send(`rendering the ${req.params.githubRepo} repo`)
 // })
 
+// search one specific movie by ?movie=<movieID>
 app.get("/search", (req, res) => {
   Movies.find({ title: req.query.movie }).then(movieDetails => {
     res.render("form-page-2", { query: movieDetails });
   });
 });
 
+// get a form page
 app.get("/form", (req, res) => {
   res.render("form-page");
 });
@@ -112,12 +116,14 @@ app.get("/movie-querystring", (req, res) => {
     });
 });
 
+// create a new movie, displaying in a select the directors
 app.get("/create-movie", (req, res) => {
   Directors.find().then(directors => {
     res.render("add-movie", { directors: directors });
   });
 });
 
+// create a new movie with its director
 app.post("/create-movie-2", (req, res) => {
   Movies.create({
     title: req.body.title,
@@ -139,31 +145,43 @@ app.post("/create-movie-2", (req, res) => {
   });
 });
 
+// delete a movie
 app.get("/movie/delete/:peliId", (req, res) => {
   Movies.findByIdAndDelete(req.params.peliId).then(deletedMovie => {
     res.redirect("/master");
   });
 });
 
+// add a test director
 app.get("/add-director", (req, res) => {
   Directors.create({
-    name: "Christopher Noland",
+    name: "Fernando Meirelles",
     movies: ["5d7775a51be232a0c7086d6e"]
+  }, {
+    name: "Christopher Noland",
+    movies: ["5d7775a51be232a0c7086d70"]
   }).then(created => {
     res.json(created);
   });
 });
 
+// show all the directors. Comment and uncomment the '.populate("movies")' statement
+// to unwrap the related movie ID using the mongo / mongoose populate capabilities
 app.get("/show-directors", (req, res) => {
   Directors.find()
     .populate("movies")
     .then(directors => {
+      // uncomment these lines should you want to transform the returned payload
+      // as per Mr. Valderrama questions
       // directors = JSON.parse(JSON.stringify(directors));
       // directors[0].movies = directors[0].movies.map(movie => movie.title);
+
+      // here we return the directors (with or without unwrapped movies with the populate)
       res.json(directors);
     });
 });
 
+// here we update a movie with the new data coming from the form
 app.post("/movie-update", (req, res) => {
   Movies.findByIdAndUpdate(req.body._id, req.body).then(updatedMovie => {
     res.redirect(`/movie/${req.body._id}`);
